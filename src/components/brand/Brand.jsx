a203af8ff8657333095cfb409147709a0043bf7e
Shamim Anowar <shamim@bildnw.com>
2022-12-25 00:40:54 +0600

    working on whatgpt feature card

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./brand.css";
+import { google, atlassian, dropbox, slack, shopify } from "./Imports";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__margin">
+      <div>
+        <img src={google} alt="" />
+      </div>
+      <div>
+        <img src={slack} alt="" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
