2ccf53ec4cfe653547e068531a589e81a655774e
Shamim Anowar <shamim@bildnw.com>
2022-12-25 16:11:43 +0600

    feature card is complete from me

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./feature.css";
 
-const Feature = () => {
-  return <div>Feature</div>;
+const Feature = ({title, subTitle}) => {
+  return <div className="gpt3__feature">
+    <div className="gpt3__feature-heading__bar"></div>
+    <h1 className="">{title}</h1>
+    <p>{subTitle} </p>
+  </div>;
 };
 
 export default Feature;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,7 +6,8 @@ const WhatGPT3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id="whatGPT3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature title={"What is GPT-3"} />
+        <div>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</div>
       </div>
       <div className="gpt3__whatgpt3-heading">
         <h1 className="gradient__text">
@@ -15,9 +16,9 @@ const WhatGPT3 = () => {
         <p>Explore The Library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature title={"Chatbots"} subTitle={"We so opinion friends me message as delight. Whole front do of plate heard oh ought."}/>
+        <Feature title={"Knowledgebase"} subTitle={"At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"}/>
+        <Feature title={"Education"} subTitle={"At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"}/>
       </div>
     </div>
   );
