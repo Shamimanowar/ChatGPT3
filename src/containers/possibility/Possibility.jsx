fbde6a8acd15a07a9d93176f15e5ee945c233673
Shamim Anowar <shamim.hire@gmail.com>
2022-12-28 12:38:58 +0600

    Possibility is added with responsive

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import possibility from "../../assets/possibility.png";
 import "./possibility.css";
 
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className="gpt3__possibility-container section__margin">
+      <div className="gpt3__possibility-image__wrapper">
+        <img src={possibility} alt="Possibility" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <p>Request Early Access to Get Started</p>
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <span>Request Early Access to Get Started</span>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
