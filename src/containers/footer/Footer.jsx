0fd8a6bf8a6018dd56f77fd55a610daa3622311c
Shamim Anowar <shamim.hire@gmail.com>
2023-01-28 01:37:28 +0600

    footer markup is going on

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -2,7 +2,28 @@ import React from "react";
 import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="gpt3__footer  section__padding">
+      <div className="gpt3__footer-heading">
+        <h1>Do you want to step in to the future before others</h1>
+        <button>Request to login</button>
+      </div>
+
+      <div className="gpt3__footer-container">
+        <div className="gpt3__footer-container_left">
+          <div>
+            <img />
+          </div>
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="gpt3__footer-container_right">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
